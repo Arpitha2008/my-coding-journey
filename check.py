44006f9d3e0af209fd9855173331e893d0f9b03f
Arpitha2008 <arpithakulal0605@gmail.com>
2026-09-16 19:32:01 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,12 @@
+# Ask user for input
+fruit = input("Enter a fruit name: ")
+
+if fruit == "Apple":
+    print("It's Apple! Red and juicy!")
+elif fruit == "Banana":
+    print("It's Banana! Yellow!")
+else:
+    print(f"Oh, {fruit} is different! Nice!")
+
+print("Thanks for playing!")
 
